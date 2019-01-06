f4711dbaf6f6e14b51345948398ef29630689318
Morgane GUERREAU <mguerrea@student.42.fr>
2019-01-06 18:55:24 +0100

    norme + auteur

--- libft/ft_lstlast.c
+++ libft/ft_lstlast.c
@@ -6,7 +6,7 @@
 /*   By: mguerrea <mguerrea@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/21 21:10:45 by mguerrea          #+#    #+#             */
-/*   Updated: 2019/01/06 17:43:17 by mguerrea         ###   ########.fr       */
+/*   Updated: 2019/01/06 18:54:59 by mguerrea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,6 @@
 t_list	*ft_lstlast(t_list *list)
 {
 	while (list != NULL && list->next != NULL)
-		list = list->next; 
+		list = list->next;
 	return (list);
 }
